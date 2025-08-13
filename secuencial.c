af50a81442a1de253dd54e80c16511f2ce5a6198
JPS4321 <sol22102@uvg.edu.gt>
2025-08-13 13:17:31 -0600

    Feat: Just 1 intersection

--- secuencial.c
+++ secuencial.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
 
-#define TOTAL_INTERSECCIONES 4
-#define DURACION_VERDE 2
-#define CARROS_POR_CALLE 1000
-#define VEHICULOS_POR_CICLO 3
-#define BUFFER_SIZE 65536  
+#define NUM_SEMAFOROS 14
+#define DURACION_CAMBIO 2
+#define CARROS_POR_CALLE 3000
+#define VEHICULOS_POR_CICLO 50
+#define BUFFER_SIZE 2000000
 
 typedef struct {
     int id;
@@ -16,7 +17,6 @@ typedef struct {
 typedef struct {
     Vehiculo** cola;
     int cantidad_carros;
-    int capacidad;
 } Calle;
 
 typedef struct {
@@ -24,150 +24,112 @@ typedef struct {
 } Semaforo;
 
 typedef struct {
-    Calle calles[4];
-    Semaforo semaforos[4];
-    int id;
+    Calle calles[NUM_SEMAFOROS];
+    Semaforo semaforos[NUM_SEMAFOROS];
 } Interseccion;
 
-void inicializarInterseccion(Interseccion* interseccion, int id_interseccion) {
-    interseccion->id = id_interseccion;
-    for (int i = 0; i < 4; i++) {
-        interseccion->calles[i].cantidad_carros = 0;
-        interseccion->calles[i].capacidad = CARROS_POR_CALLE;
-        interseccion->calles[i].cola = malloc(CARROS_POR_CALLE * sizeof(Vehiculo*));
+void inicializarInterseccion(Interseccion* inter) {
+    for (int i = 0; i < NUM_SEMAFOROS; i++) {
+        inter->calles[i].cantidad_carros = CARROS_POR_CALLE;
+        inter->calles[i].cola = malloc(CARROS_POR_CALLE * sizeof(Vehiculo*));
         for (int j = 0; j < CARROS_POR_CALLE; j++) {
             Vehiculo* v = malloc(sizeof(Vehiculo));
             v->id = i * 100 + j;
-            interseccion->calles[i].cola[j] = v;
-            interseccion->calles[i].cantidad_carros++;
+            inter->calles[i].cola[j] = v;
+        }
+        inter->semaforos[i].estado = rand() % 3; 
+    }
+}
+
+void imprimirSemaforos(Interseccion* inter, char* buffer, size_t* offset) {
+    const char* estados[] = {"ROJO", "AMARILLO", "VERDE"};
+    for (int i = 0; i < NUM_SEMAFOROS; i++) {
+        if (*offset < BUFFER_SIZE - 1) {
+            *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
+                                "Semaforo %d: %s\n", i + 1, estados[inter->semaforos[i].estado]);
         }
     }
-    for (int i = 0; i < 4; i++) {
-        interseccion->semaforos[i].estado = 0;
-    }
-    interseccion->semaforos[0].estado = 1;
-    interseccion->semaforos[2].estado = 1;
 }
 
-void imprimirSemaforosBuffer(Interseccion* interseccion, char* buffer, size_t* offset) {
-    const char* nombres[4] = {"Norte", "Este", "Sur", "Oeste"};
-    const char* estados[2] = {"ROJO", "VERDE"};
-    *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
-                        "\n[Interseccion %d] ESTADO DE SEMAFOROS:\n", interseccion->id);
-    for (int i = 0; i < 4; i++) {
-        *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
-                            " Calle %s: %s\n", nombres[i], estados[interseccion->semaforos[i].estado]);
-    }
-}
-
-void imprimirCallesBuffer(Interseccion* interseccion, char* buffer, size_t* offset) {
-    const char* nombres[4] = {"Norte", "Este", "Sur", "Oeste"};
-    *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
-                        "\n[Interseccion %d] VEHICULOS EN CADA CALLE:\n", interseccion->id);
-    for (int i = 0; i < 4; i++) {
-        *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
-                            " Calle %s (%d):", nombres[i], interseccion->calles[i].cantidad_carros);
-        for (int j = 0; j < interseccion->calles[i].cantidad_carros; j++) {
-            Vehiculo* v = interseccion->calles[i].cola[j];
-            *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset, " [%d]", v->id);
-        }
-        *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset, "\n");
-    }
-}
-
-void avanzarSemaforos(Interseccion* interseccion, int* par_actual) {
-    for (int i = 0; i < 4; i++) {
-        interseccion->semaforos[i].estado = 0;
-    }
-    if (*par_actual == 0) {
-        interseccion->semaforos[1].estado = 1;
-        interseccion->semaforos[3].estado = 1;
-        *par_actual = 1;
-    } else {
-        interseccion->semaforos[0].estado = 1;
-        interseccion->semaforos[2].estado = 1;
-        *par_actual = 0;
-    }
-}
-
-void moverVehiculosBuffer(Interseccion* interseccion, int calle, char* buffer, size_t* offset) {
-    Calle* c = &interseccion->calles[calle];
-    int mover = (c->cantidad_carros < VEHICULOS_POR_CICLO) ? c->cantidad_carros : VEHICULOS_POR_CICLO;
-
-    for (int k = 0; k < mover; k++) {
-        Vehiculo* v = c->cola[0];
-        *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
-                            " [Inter %d] Vehiculo %d cruzo desde calle %d\n", interseccion->id, v->id, calle);
-        free(v);
-        for (int i = 1; i < c->cantidad_carros; i++) {
-            c->cola[i - 1] = c->cola[i];
-        }
-        c->cantidad_carros--;
-    }
-}
-
-int quedanVehiculos(Interseccion* interseccion) {
-    for (int i = 0; i < 4; i++) {
-        if (interseccion->calles[i].cantidad_carros > 0) return 1;
-    }
-    return 0;
-}
-
-void simularInterseccion(Interseccion* inter) {
-    char buffer[BUFFER_SIZE];
-    size_t offset = 0;
-
-    int par_actual = 0;
-    int tiempo_restante = DURACION_VERDE;
-    int ciclo = 1;
-
-    while (quedanVehiculos(inter)) {
-        offset = 0;
-        offset += snprintf(buffer + offset, BUFFER_SIZE - offset,
-                           "\n=== INTERSECCION %d - CICLO %d ===\n", inter->id, ciclo++);
-
-        imprimirSemaforosBuffer(inter, buffer, &offset);
-        imprimirCallesBuffer(inter, buffer, &offset);
-
-        for (int i = 0; i < 4; i++) {
-            if (inter->semaforos[i].estado == 1) {
-                moverVehiculosBuffer(inter, i, buffer, &offset);
+void moverVehiculos(Interseccion* inter, char* buffer, size_t* offset) {
+    for (int i = 0; i < NUM_SEMAFOROS; i++) {
+        if (inter->semaforos[i].estado == 2) { 
+            int mover = (inter->calles[i].cantidad_carros < VEHICULOS_POR_CICLO)
+                        ? inter->calles[i].cantidad_carros
+                        : VEHICULOS_POR_CICLO;
+            for (int k = 0; k < mover; k++) {
+                Vehiculo* v = inter->calles[i].cola[0];
+                if (*offset < BUFFER_SIZE - 1) {
+                    *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset,
+                                        "Vehiculo %d cruzo desde calle %d\n", v->id, i + 1);
+                }
+                free(v);
+                for (int m = 1; m < inter->calles[i].cantidad_carros; m++) {
+                    inter->calles[i].cola[m - 1] = inter->calles[i].cola[m];
+                }
+                inter->calles[i].cantidad_carros--;
             }
         }
+    }
+}
 
-        printf("%s", buffer);
-
-        tiempo_restante--;
-        if (tiempo_restante == 0) {
-            avanzarSemaforos(inter, &par_actual);
-            tiempo_restante = DURACION_VERDE;
+void imprimirCalles(Interseccion* inter, char* buffer, size_t* offset) {
+    for (int i = 0; i < NUM_SEMAFOROS; i++) {
+        if (*offset < BUFFER_SIZE - 1) {
+            *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset, "Calle %d:", i + 1);
+        }
+        for (int j = 0; j < inter->calles[i].cantidad_carros; j++) {
+            if (*offset < BUFFER_SIZE - 1) {
+                *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset, " %d", inter->calles[i].cola[j]->id);
+            }
+        }
+        if (*offset < BUFFER_SIZE - 1) {
+            *offset += snprintf(buffer + *offset, BUFFER_SIZE - *offset, "\n");
         }
     }
-    printf("[Interseccion %d] Todos los vehiculos han cruzado.\n", inter->id);
+}
 
-    for (int i = 0; i < 4; i++) {
-        free(inter->calles[i].cola);
+void actualizarSemaforos(Interseccion* inter, int ciclo) {
+    if (ciclo % DURACION_CAMBIO == 0) {
+        for (int i = 0; i < NUM_SEMAFOROS; i++) {
+            inter->semaforos[i].estado = (inter->semaforos[i].estado + 1) % 3;
+        }
     }
+}
+
+int contarVehiculos(Interseccion* inter) {
+    int total = 0;
+    for (int i = 0; i < NUM_SEMAFOROS; i++) {
+        total += inter->calles[i].cantidad_carros;
+    }
+    return total;
+}
+
+void simularInterseccionSecuencial(Interseccion* inter, int num_ciclos) {
+    char* buffer = malloc(BUFFER_SIZE);
+    for (int ciclo = 1; ciclo <= num_ciclos && contarVehiculos(inter) > 0; ciclo++) {
+        size_t offset = 0;
+        offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\n--- Ciclo %d ---\n", ciclo);
+
+        actualizarSemaforos(inter, ciclo);
+        imprimirSemaforos(inter, buffer, &offset);
+        moverVehiculos(inter, buffer, &offset);
+        imprimirCalles(inter, buffer, &offset);
+
+        printf("%s", buffer);
+    }
+    free(buffer);
 }
 
 int main() {
-    printf("Inicio de simulacion SECUENCIAL\n");
     srand(time(NULL));
-    Interseccion intersecciones[TOTAL_INTERSECCIONES];
+    Interseccion inter;
+    inicializarInterseccion(&inter);
 
-    for (int i = 0; i < TOTAL_INTERSECCIONES; i++) {
-        inicializarInterseccion(&intersecciones[i], i);
-    }
+    double start_time = (double)clock() / CLOCKS_PER_SEC;
+    simularInterseccionSecuencial(&inter, 200);
+    double end_time = (double)clock() / CLOCKS_PER_SEC;
 
-    clock_t start_time = clock();
-
-    for (int i = 0; i < TOTAL_INTERSECCIONES; i++) {
-        simularInterseccion(&intersecciones[i]);
-    }
-
-    clock_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-    printf("\nTiempo total de simulacion: %.3f segundos\n", elapsed_time);
+    printf("\nTiempo total de simulacion: %.3f segundos\n", end_time - start_time);
     return 0;
 }
